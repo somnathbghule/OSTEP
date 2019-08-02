587617d58c5a0fc8faa03de17d4d8e6ea883965a
Somnath Ghule <somnathbghule@gmail.com>
2019-08-02 22:33:45 +0530

    basic shell added.

--- lab-projects-systems/shell/shell.c
+++ lab-projects-systems/shell/shell.c
@@ -1,10 +1,34 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <shell.h>
+#include <unistd.h>
+
+
+int executeShell(const char *argv){
+
+	int fd;
+	fd=fork();
+
+	if( fd == 0 ){ //new child process
+		execlp(argv, argv, NULL);
+
+	}
+	
+	return 0;
+}
+
 
 int main(int argc, char **argv) {
 
-	dprintf("argc: %d\n", argc);
+
+	char buf[512];
+
+	while (1){
+		printf("\nsh$ ");
+		scanf("%s",buf);	
+		executeShell(buf);
+	}
+
 	//return 0
 	return (0);
 }
