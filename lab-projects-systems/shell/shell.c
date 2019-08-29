f68679f0fef19d2f5fb8d76a5ffca96b21a63a03
Somnath Ghule <somnathbghule@gmail.com>
2019-08-29 20:40:51 +0530

    cmd options added.

--- lab-projects-systems/shell/shell.c
+++ lab-projects-systems/shell/shell.c
@@ -3,23 +3,31 @@
 #include <shell.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <string.h>
 
 typedef struct cmd {
 	char bin[32];
 	char argv[1024];
 }cmd;
 
-int parseCmd(const char *argv){
+int parseCmd(const char *argv, cmd *c){
 	int i = 0;
-	while(argv[i]){
-		if( i==0 ){
-			
+
+	while( argv[i] ) {
+		if( i==0 ) {
+			while ( argv[i] && argv[i] != ' ' ) {
+				c->bin[i] = argv[i];
+				i++;		
+			}	         			  		
+		}else if (*(argv+i-1) ){
+			strcpy(c->argv, argv+i+1);	
+			break;
 		}
-		i++;
 	}
+	return 0;
 }
 
-int executeShell(const char *argv){
+int executeShell(cmd c){
 
 	int cfd;
 	int status = 0;
@@ -27,7 +35,10 @@ int executeShell(const char *argv){
 	int wstatus=0;
 
 	if( cfd == 0 ){ //new child process
-		status = execlp(argv, argv, NULL);
+		if (*c.argv)
+			status = execlp(c.bin, c.bin, c.argv, NULL);
+		else
+			status = execlp(c.bin, c.bin, NULL, NULL);
 
 	}
 	wait(&wstatus);
@@ -39,14 +50,19 @@ int main(int argc, char **argv) {
 
 
 	char buf[512];
-
+	cmd c;
+	c.bin[0]='\0';
+	c.argv[0]='\0';
 	while (1){
 
 		printf("sh$ ");
 		//scanf("%[^\n]",buf);	
-		gets(buf);	
-		printf("inputwas:%s\n",buf);
-		executeShell(buf);
+		gets(buf);
+		printf("inputwas:\"%s\"\n",buf);
+		parseCmd(buf, &c);	
+		executeShell(c);
+		c.bin[0]='\0';
+		c.argv[0]='\0';
 	}
 
 	//return 0
